bec3571e89d76345482bcb7b26f78609b71e37fe
deepika <deepdadore18@gmail.com>
2025-06-17 15:27:59 +0530

    pagination

--- controllers/userProductController.js
+++ controllers/userProductController.js
@@ -1,16 +1,19 @@
 const Product = require('../models/productmodel');
 const express = require('express');
 const fetchproduct = async (req,res)=>{
-    const {category , price}= req.query //req.body, req.params.id
-    let filter = {}
-    if(category){
-        filter.category = category
-    }
-    if(price){
-        filter.price = price
-    }
-    const data = await Product.find(filter)
-    res.render('product',{data})
+
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 5
+    const skip = (page-1)*limit
+    const total = await Product.countDocuments()
+
+    const data = await Product.find().skip(skip).limit(limit)
+    res.render('product',{
+        data,
+        currentpage:page,
+        totalpages: Math.ceil(total/limit),
+        limit
+    })
 }
 
 module.exports = fetchproduct
